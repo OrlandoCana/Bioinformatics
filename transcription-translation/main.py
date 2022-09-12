1d53761e847ee05dc0d02f49e7c3fdb551bc6a05
OrlandoCana <160888@unsaac.edu.pe>
2022-09-12 04:37:12 -0500

    Adding main.py file where the modules test is done

---
+++ transcription-translation/main.py
@@ -0,0 +1,20 @@
+from msilib import sequence
+import functions as func
+if (__name__ == '__main__'):
+    filePath = 'transcription-translation/sequence.fasta'
+    sequences, headers = func.read_file(filePath)
+    print(f'This file contains {len(sequences)} sequences')
+    print('This is the first sequence:')
+    print(sequences[0])
+    print()
+    
+    print('the first strand of DNA converted to complementary ' + 
+          'strand of RNA')
+    arn = func.transcription(sequences[0])
+    print(arn)
+    print()
+    
+    print('the first strand of complementary ARN converted ' + 
+          'to strand of proteins')
+    proteins = func.translation(arn)
+    print(proteins)
